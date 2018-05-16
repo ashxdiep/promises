8d7fa4a90126e5cf70ec611a0fb54d618b8f5251
ash.d.321@gmail.com <ash.d.321@gmail.com>
2018-05-16 15:13:18 -0500

    callbacks

--- main.js
+++ main.js
@@ -26,11 +26,63 @@
 //     });
 
 
-function sync(){
-  setTimeout(deferred, 1000);
-  console.log('sync');
+function sync(callback){
+  //what set timeout does, is say don't do the function now, do it later
+  //but not earlier than this time
+
+  //no such thing as setTimeout(return deferred() , 0); because cannot get immediately
+  //sync needs to now take a callback function
+  setTimeout(function(){
+    const res = deferred();
+    callback(res);
+  }, 1000);
 }
 
 function deferred() {
-  console.log('deferred');
+  return 'deferred';
 }
+
+//call sync first and send in anon callback function
+//call setTimeout with function that wraps the deferred call
+//call back is called with result which comes out in the function of sync we called first
+//.and it logs out
+sync(function(result){
+  console.log(result);
+});
+
+
+/////ANOTHER EXAMPLE/////
+/////////////////////////
+//program that calculates disposable income
+
+function getSalary(callback) {
+    setTimeout(() => {
+      callback(33000);
+    }, 1000);
+}
+
+function subtractTax(salary, callback) {
+    setTimeout(() => {
+      callback(salary * 0.75);
+    }, 1000);
+}
+
+function subtractRent(salary, callback) {
+  setTimeout(() => {
+    callback(salary - 5000);
+  }, 1000);
+}
+
+function getDisposableIncome() {
+    getSalary(salary1 =>{
+      subtractTax(salary1, salary2 =>{
+        subtraactRent(salary2, salary3 =>{
+          callback(salary3);
+        });
+      });
+    });
+}
+
+getDisposableIncome(disposableincome =>{
+  console.log(disposableincome);
+})
